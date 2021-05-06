3acf5ef888d8d42fca7888b90e80b285f034ad4c
G Soros <gsoros@users.noreply.github.com>
2021-05-06 22:34:18 +0200

    client fetch config

--- client/client.ino
+++ client/client.ino
@@ -2,7 +2,7 @@
 #include <WiFiClient.h>
 #include <Scheduler.h>                  // https://github.com/nrwiersma/ESP8266Scheduler
 #include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
+#include <ArduinoJson.h>                // https://github.com/bblanchon/ArduinoJson
 
 const char *AP_SSID = "StepperControl";
 const char *AP_PASSWORD = NULL;
@@ -11,15 +11,11 @@ const IPAddress DEFAULT_GATEWAY(192, 168, 4, 1);
 const int POT_PIN = A0;
 const int POT_MIN = 0;
 const int POT_MAX = 1024;
-const int MOVEMENT_MIN = 2;             // minimum movement of the pot percentage
+const int MOVEMENT_MIN = 2;             // minimum movement of the pot
 const int MEASUREMENTS_PER_SEC = 10;    // maximum number of pot measurements per second
-const int COMMANDS_PER_SEC = 10;        // maximum number of commands per second
-const int COMMAND_MIN = -511;           // TODO get these params from the server
-const int COMMAND_MAX = 512;
 const int NUM_MEASUREMENTS = 64;        // number of analog measurements to average
 const int MEASUREMENT_MAX = 1024 * NUM_MEASUREMENTS;
 const int MEASUREMENT_DELAY = 1000 / MEASUREMENTS_PER_SEC;
-const int COMMAND_DELAY = 1000 / COMMANDS_PER_SEC;
 
 const char *wl_status[] = {
     /*0*/ "idle", 
@@ -31,20 +27,48 @@ const char *wl_status[] = {
     /*6*/ "disconnected"
 };
 
+int command_rate = 500;
+int command_min = -100;
+int command_max = 100;
 int measurement_total;
 int measurement = (POT_MIN + POT_MAX) / 2;
 int measurement_tmp = measurement;
-int last_command = (COMMAND_MIN + COMMAND_MAX) / 2;
+int last_command = (command_min + command_max) / 2;
 int command = last_command;
 int command_diff = 0;
-bool monitor = false;
+bool monitor_enable = false;
 IPAddress host = DEFAULT_GATEWAY;
+int responseBufferLength = 255;
 
 void waitForWifi(int pause = 100) {
     while (WiFi.status() != WL_CONNECTED) {
         delay(pause);
     }
 }
+
+int httpRequest(char *url, char *response) {
+    int http_code;
+    WiFiClient client;
+    HTTPClient http;
+    if (http.begin(client, url)) {     
+        Serial.printf("[HTTP] GET %s\n", url);
+        http_code = http.GET();
+        if (http_code <= 0) {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(http_code).c_str());
+        } else {
+            Serial.printf("[HTTP] GET... code: %d\n", http_code);
+            if (http_code == HTTP_CODE_OK || http_code == HTTP_CODE_MOVED_PERMANENTLY) {
+                snprintf(response, responseBufferLength, http.getString().c_str());
+                Serial.printf("[HTTP] Response: %s\n", response);
+            }      
+        }         
+        http.end();
+    } else {
+        Serial.print("[HTTP] Unable to connect\n");
+    }
+    return http_code;    
+}
+
 
 class MeasureTask : public Task {
     protected:
@@ -94,39 +118,40 @@ class CommandTask : public Task {
             WiFi.localIP().toString().c_str(), 
             host.toString().c_str()
         );
-        monitor = true;
+        char url[100];
+        sprintf(url, "http://%s/config", host.toString().c_str());
+        char response[responseBufferLength];
+        int http_code = httpRequest(url, response);
+        if (http_code == HTTP_CODE_OK) {
+            Serial.print("[Config] code OK\n");
+            StaticJsonDocument<200> conf;
+            deserializeJson(conf, response);
+            const char *confName = conf["name"];
+            Serial.printf("[Config] host is %s\n", confName);
+            Serial.printf("[Config] old values: min %i, max %i, rate %i\n", command_min, command_max, command_rate);
+            command_min = conf["min"];
+            command_max = conf["max"];
+            command_rate = conf["rate"];
+            Serial.printf("[Config] new values: min %i, max %i, rate %i\n", command_min, command_max, command_rate);
+        }
+        monitor_enable = true;
     }
     void loop() {
         waitForWifi();
-        command = map(measurement, POT_MIN, POT_MAX, COMMAND_MIN, COMMAND_MAX);
+        command = map(measurement, POT_MIN, POT_MAX, command_min, command_max);
         command_diff = abs(last_command - command);
         if (command_diff > MOVEMENT_MIN) {
             last_command = command;
             Serial.printf("Sending command: %d\n", command);
-            WiFiClient client;
-            HTTPClient http;
             char url[100];
             sprintf(url, "http://%s/command?vector=%d", host.toString().c_str(), command);
-            if (http.begin(client, url)) {  // HTTP       
-                Serial.printf("[HTTP] GET %s\n", url);
-                int httpCode = http.GET();
-                if (httpCode <= 0) {
-                    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-                } else {
-                    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-                    if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-                        Serial.printf("[HTTP] Response: %s\n", http.getString().c_str());
-                    }      
-                }         
-                http.end();
-            } else {
-                Serial.print("[HTTP] Unable to connect\n");
-            }         
+            char response[responseBufferLength];
+            httpRequest(url, response);   
         } 
         else if (command_diff > 0) {
             //Serial.printf("%d movement too small\n", command_diff); 
         }
-        delay(COMMAND_DELAY);
+        delay(command_rate);
     }
 } command_task;
 
@@ -134,7 +159,7 @@ class CommandTask : public Task {
 class MonitorTask : public Task {
     protected:
     void loop() {
-        while (!monitor) {
+        while (!monitor_enable) {
             delay(100); 
         }
         Serial.printf(
